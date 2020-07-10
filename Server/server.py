c11734a8c224f05acacdbcadbf598f94173c7453
Ali <alijfri99@gmail.com>
2020-07-11 00:49:38 +0430

    Query No. 1 is now working

--- Server/server.py
+++ Server/server.py
@@ -14,18 +14,20 @@ db = mysql.connector.connect(
 cursor = db.cursor()
 
 def addtolist(cur,result,queryType):
-	mydict = {}
 	for i in cur:
 		if(queryType==1 or queryType==2 or queryType==3):
+			mydict = {}
 			mydict["name"] = i[0]
 			mydict["type"] = i[1]
 			result.append(mydict)
 		elif(queryType==4 or queryType==5):
+			mydict = {}
 			mydict["name"] = i[0]
 			mydict["type"] = i[1]
 			mydict["artist"] = i[2]
 			result.append(mydict)
 		elif(queryType==6):
+			mydict = {}
 			mydict["name"] = i[0]
 			mydict["type"] = i[1]
 			mydict["owner"] = i[2]
@@ -39,23 +41,21 @@ def search():
 	query = "select username,'user' as source from user where username like '%" + str(name) +  "%'"
 	cursor.execute(query)
 	addtolist(cursor, a, 1)
-	'''
-	query =f"select username,'listener' as source from listener where firstname like "%{name}%" or lastname like "%{name}%""
+	query ="select username,'listener' as source from listener where firstname like '%" + str(name) +  "%' or lastname like '%" + str(name) +  "%'"
 	cursor.execute(query)
-	addtolist(cursor, a)
-	query = f"select username,'artist' as source from artist where artisticname like "%{name}%""
+	addtolist(cursor, a, 2)
+	query = "select username,'artist' as source from artist where artisticname like '%" + str(name) +  "%'"
 	cursor.execute(query)
-	addtolist(cursor, a)
-	query = f"select title,'song' as source ,artist from song where title like "%{name}%""
+	addtolist(cursor, a, 3)
+	query = "select title,'song' as source ,artist from song where title like '%" + str(name) +  "%'"
 	cursor.execute(query)
-	addtolist(cursor, a)
-	query = f"select title,'album' as source ,artist from album where title like "%{name}%""
+	addtolist(cursor, a, 4)
+	query = "select title,'album' as source ,artist from album where title like '%" + str(name) +  "%'"
 	cursor.execute(query)
-	addtolist(cursor, a)
-	query = f"select title,'playlist' as source ,username from playlist where title like "%{name}%""
+	addtolist(cursor, a, 5)
+	query = "select title,'playlist' as source ,username from playlist where title like '%" + str(name) +  "%'"
 	cursor.execute(query)
-	addtolist(cursor, a)
-	'''
+	addtolist(cursor, a, 6)
 	jsonObj = json.dumps(a)
 	return jsonObj
 
