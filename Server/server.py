8a33a03a01ae9177d0b9cc342fcdc9c0ec8df0cb
Ali <alijfri99@gmail.com>
2020-07-11 00:07:50 +0430

    Fixing the server

--- Server/server.py
+++ Server/server.py
@@ -1,13 +1,14 @@
 from flask import Flask ,redirect,url_for,render_template,request
 import mysql.connector
+import json
 
 app = Flask(__name__)
 
 db = mysql.connector.connect(
         host = "localhost",
         user = "root",
-        password = "anymistake",
-        database = "database_project"
+        password = "ghon",
+        database = "dbproject"
     )
 
 cursor = db.cursor()
@@ -17,14 +18,15 @@ def addtolist(cur,a):
 		a.append(i)
 
 
-
-@app.route("/search")
 a=[]
+@app.route("/search")
 def search():
+	a = []
 	name = request.args.get("name")
-	query = f"select username,'user' as source from user where username like "%{name}%"" 
+	query = "select username,'user' as source from user where username like '%" + str(name) +  "%'"
 	cursor.execute(query)
 	addtolist(cursor, a)
+	'''
 	query =f"select username,'listener' as source from listener where firstname like "%{name}%" or lastname like "%{name}%""
 	cursor.execute(query)
 	addtolist(cursor, a)
@@ -40,7 +42,12 @@ def search():
 	query = f"select title,'playlist' as source ,username from playlist where title like "%{name}%""
 	cursor.execute(query)
 	addtolist(cursor, a)
-	
+	'''
+	jsonObj = json.dumps(a)
+	return jsonObj
+
+if __name__ == "__main__":
+	app.run(host ="localhost" , port =5000,debug=True)
 
 
 
